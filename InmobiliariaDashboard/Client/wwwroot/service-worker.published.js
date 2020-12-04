a07b8b802376ee607479ee9fcae130d8e46cecbc
Marco Antonio <marco.3292@gmail.com>
2020-12-04 00:59:11 -0600

    Minor style and service worker fixes

--- InmobiliariaDashboard/Client/wwwroot/service-worker.published.js
+++ InmobiliariaDashboard/Client/wwwroot/service-worker.published.js
@@ -1,6 +1,7 @@
 // Caution! Be sure you understand the caveats before publishing an application with
 // offline support. See https://aka.ms/blazor-offline-considerations
 
+self.importScripts('./common.js');
 self.importScripts('./service-worker-assets.js');
 self.addEventListener('install', event => event.waitUntil(onInstall(event)));
 self.addEventListener('activate', event => event.waitUntil(onActivate(event)));
