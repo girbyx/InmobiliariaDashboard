9b6b0880913c642ae87caf39129cf2295322c819
Marco Antonio <marco.3292@gmail.com>
2020-12-05 16:07:08 -0600

    Add project view model

---
+++ InmobiliariaDashboard/Shared/ViewModels/ProjectViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace InmobiliariaDashboard.Shared.ViewModels
+{
+    public class ProjectViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime StartDate { get; set; } = DateTime.Now;
+        public DateTime? EndDate { get; set; }
+        public int ClientId { get; set; }
+        public int ClientName { get; set; }
+
+        // select lists
+        public IEnumerable<ClientViewModel> Clients { get; set; } = new List<ClientViewModel>();
+    }
+}
