b7a9ab17fef2856de6aec6f5f1d496a782f9d7d4
Marco Antonio <marco.3292@gmail.com>
2020-12-05 16:07:21 -0600

    Add client services

---
+++ InmobiliariaDashboard/Server/Services/BaseService.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using AutoMapper;
+using InmobiliariaDashboard.Server.Data;
+
+namespace InmobiliariaDashboard.Server.Services
+{
+    public interface IBaseService<out TViewModel> where TViewModel : class
+    {
+        IEnumerable<TViewModel> GetAll();
+    }
+
+    public class BaseService<TEntity, TViewModel> : IBaseService<TViewModel>
+        where TEntity : class
+        where TViewModel : class
+    {
+        private readonly IApplicationDbContext _dbContext;
+        private readonly IMapper _mapper;
+
+        public BaseService(IApplicationDbContext dbContext, IMapper mapper)
+        {
+            _dbContext = dbContext;
+            _mapper = mapper;
+        }
+
+        public IEnumerable<TViewModel> GetAll()
+        {
+            return _dbContext.Set<TEntity>().ToList().Select(_mapper.Map<TViewModel>);
+        }
+    }
+}
---
+++ InmobiliariaDashboard/Server/Services/ClientService.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using InmobiliariaDashboard.Server.Data;
+using InmobiliariaDashboard.Shared.ViewModels;
+
+namespace InmobiliariaDashboard.Server.Services
+{
+    public interface IClientService : IBaseService<ClientViewModel>
+    {
+    }
+
+    public class ClientService : BaseService<Models.Client, ClientViewModel>, IClientService
+    {
+        public ClientService(IApplicationDbContext dbContext, IMapper mapper) : base(dbContext, mapper)
+        {
+        }
+    }
+}
