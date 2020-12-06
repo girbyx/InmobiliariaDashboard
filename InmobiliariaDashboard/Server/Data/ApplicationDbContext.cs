b1492217871f66405d5002c0b4ca56ef4abe71ff
Marco Antonio <marco.3292@gmail.com>
2020-12-06 17:28:38 -0600

    Restore migrations and build an explicit schema creation

--- InmobiliariaDashboard/Server/Data/ApplicationDbContext.cs
+++ InmobiliariaDashboard/Server/Data/ApplicationDbContext.cs
@@ -34,6 +34,44 @@ namespace InmobiliariaDashboard.Server.Data
         {
         }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Account>().HasOne(p => p.Client).WithMany(b => b.Accounts).IsRequired();
+            modelBuilder.Entity<Account>().HasMany(p => p.Gains).WithOne(b => b.Account);
+            modelBuilder.Entity<Account>().HasMany(p => p.Costs).WithOne(b => b.Account);
+            modelBuilder.Entity<Account>().HasMany(p => p.Losses).WithOne(b => b.Account);
+
+            modelBuilder.Entity<Attachment>().HasOne(p => p.Gain).WithMany(b => b.Attachments);
+            modelBuilder.Entity<Attachment>().HasOne(p => p.Cost).WithMany(b => b.Attachments);
+            modelBuilder.Entity<Attachment>().HasOne(p => p.Loss).WithMany(b => b.Attachments);
+
+            modelBuilder.Entity<Models.Client>().HasMany(p => p.Projects).WithOne(b => b.Client);
+            modelBuilder.Entity<Models.Client>().HasMany(p => p.Contacts).WithOne(b => b.Client);
+            modelBuilder.Entity<Models.Client>().HasMany(p => p.Accounts).WithOne(b => b.Client);
+
+            modelBuilder.Entity<Contact>().HasOne(p => p.Client).WithMany(b => b.Contacts).IsRequired();
+
+            modelBuilder.Entity<Cost>().HasOne(p => p.CostType).WithMany(b => b.Costs).IsRequired();
+            modelBuilder.Entity<Cost>().HasOne(p => p.Project).WithMany(b => b.Costs).IsRequired();
+            modelBuilder.Entity<Cost>().HasOne(p => p.Account).WithMany(b => b.Costs).IsRequired();
+            modelBuilder.Entity<Cost>().HasMany(p => p.Attachments).WithOne(b => b.Cost);
+
+            modelBuilder.Entity<Gain>().HasOne(p => p.GainType).WithMany(b => b.Gains).IsRequired();
+            modelBuilder.Entity<Gain>().HasOne(p => p.Project).WithMany(b => b.Gains).IsRequired();
+            modelBuilder.Entity<Gain>().HasOne(p => p.Account).WithMany(b => b.Gains).IsRequired();
+            modelBuilder.Entity<Gain>().HasMany(p => p.Attachments).WithOne(b => b.Gain);
+
+            modelBuilder.Entity<Loss>().HasOne(p => p.LossType).WithMany(b => b.Losses).IsRequired();
+            modelBuilder.Entity<Loss>().HasOne(p => p.Project).WithMany(b => b.Losses).IsRequired();
+            modelBuilder.Entity<Loss>().HasOne(p => p.Account).WithMany(b => b.Losses).IsRequired();
+            modelBuilder.Entity<Loss>().HasMany(p => p.Attachments).WithOne(b => b.Loss);
+
+            modelBuilder.Entity<Project>().HasOne(p => p.Client).WithMany(b => b.Projects).IsRequired();
+            modelBuilder.Entity<Project>().HasMany(p => p.Gains).WithOne(b => b.Project);
+            modelBuilder.Entity<Project>().HasMany(p => p.Costs).WithOne(b => b.Project);
+            modelBuilder.Entity<Project>().HasMany(p => p.Losses).WithOne(b => b.Project);
+        }
+
         public override EntityEntry<TEntity> Add<TEntity>(TEntity entity)
         {
             if (entity is IAuditFields)
