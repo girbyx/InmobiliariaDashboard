7d56ab463d4b7336cb93cd785d562a5b266f9552
Marco Antonio <marco.3292@gmail.com>
2020-12-06 17:39:41 -0600

    Add new project service

---
+++ InmobiliariaDashboard/Server/Services/ProjectService.cs
@@ -0,0 +1,16 @@
+﻿using InmobiliariaDashboard.Server.Data;
+using InmobiliariaDashboard.Server.Models;
+
+namespace InmobiliariaDashboard.Server.Services
+{
+    public interface IProjectService : IBaseService<Project>
+    {
+    }
+
+    public class ProjectService : BaseService<Project>, IProjectService
+    {
+        public ProjectService(IApplicationDbContext dbContext) : base(dbContext)
+        {
+        }
+    }
+}
