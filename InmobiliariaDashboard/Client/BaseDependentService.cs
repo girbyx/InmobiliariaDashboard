b0a3fb088f0690dcfef95de863f5a51bc4e11ce7
Marco Antonio <marco.3292@gmail.com>
2020-12-07 03:47:06 -0600

    Create dependent services

---
+++ InmobiliariaDashboard/Client/BaseDependentService.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using InmobiliariaDashboard.Shared.ViewModels;
+using Microsoft.AspNetCore.Components;
+
+namespace InmobiliariaDashboard.Client
+{
+    public class BaseDependentService<TViewModel> : BaseCatalogService<TViewModel>, IBaseDependentService
+        where TViewModel : class
+    {
+        public BaseDependentService(HttpClient httpClient, NavigationManager navigationManager)
+            : base(httpClient, navigationManager)
+        {
+        }
+
+        public async Task<IEnumerable<AccountViewModel>> GetAccountsByProject(int id)
+        {
+            return await HttpClient.GetFromJsonAsync<IEnumerable<AccountViewModel>>($"api/Account/ByProject?id={id}");
+        }
+    }
+}
---
+++ InmobiliariaDashboard/Client/IBaseDependentService.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using InmobiliariaDashboard.Shared.ViewModels;
+
+namespace InmobiliariaDashboard.Client
+{
+    public interface IBaseDependentService
+    {
+        Task<IEnumerable<AccountViewModel>> GetAccountsByProject(int id);
+    }
+}
