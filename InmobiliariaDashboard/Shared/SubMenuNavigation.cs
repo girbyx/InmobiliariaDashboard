141964a7c010d9b96c6a6901cd28b7d0a0e2d8cc
Marco Antonio <marco.3292@gmail.com>
2020-12-12 16:09:50 -0600

    Add project sub menu

--- InmobiliariaDashboard/Shared/SubMenuNavigation.cs
+++ InmobiliariaDashboard/Shared/SubMenuNavigation.cs
@@ -3,6 +3,7 @@
     public class SubMenuNavigation
     {
         public bool AssetSubMenu { get; set; }
+        public bool ProjectSubMenu { get; set; }
         public bool CostSubMenu { get; set; }
         public bool GainSubMenu { get; set; }
         public bool LossSubMenu { get; set; }
