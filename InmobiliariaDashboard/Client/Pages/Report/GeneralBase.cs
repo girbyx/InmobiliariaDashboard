714e9a62b1ceb5b84362044d2168bc1faf654ae3
Marco Antonio <marco.3292@gmail.com>
2020-12-17 02:21:19 -0600

    Add views

---
+++ InmobiliariaDashboard/Client/Pages/Report/GeneralBase.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using InmobiliariaDashboard.Shared.ReportViewModels;
+using Microsoft.AspNetCore.Components;
+
+namespace InmobiliariaDashboard.Client.Pages.Report
+{
+    public class GeneralBase : ComponentBase
+    {
+        [Inject] public IService Service { get; set; }
+        public GeneralBalanceEnterpriseViewModel Record { get; set; } = new GeneralBalanceEnterpriseViewModel();
+
+        protected override async Task OnInitializedAsync()
+        {
+            Record.Enterprises = await Service.GetEnterpriseList();
+        }
+    }
+}
