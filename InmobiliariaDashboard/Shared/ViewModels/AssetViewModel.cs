07163e0f9fd013333bfe291ce207174dcdf7e0ad
Marco Antonio <marco.3292@gmail.com>
2020-12-19 15:25:52 -0600

    Add quantity to asset view model

--- InmobiliariaDashboard/Shared/ViewModels/AssetViewModel.cs
+++ InmobiliariaDashboard/Shared/ViewModels/AssetViewModel.cs
@@ -8,6 +8,7 @@ namespace InmobiliariaDashboard.Shared.ViewModels
         public int Id { get; set; }
         [Required] public string Name { get; set; }
         [Required] public double Value { get; set; }
+        public int Quantity { get; set; }
         [Required] public string Description { get; set; }
 
         [Range(1, int.MaxValue, ErrorMessage = "Por favor, seleccione una empresa")]
