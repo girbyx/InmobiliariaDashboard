59b6c03da091da338d17b7a01b74ee65f622a9f8
Marco Antonio <marco.3292@gmail.com>
2020-12-22 15:03:15 -0600

    Improve balance view model

--- InmobiliariaDashboard/Shared/ViewModels/BalanceViewModel.cs
+++ InmobiliariaDashboard/Shared/ViewModels/BalanceViewModel.cs
@@ -4,10 +4,11 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public double AssetValue { get; set; }
+        public double ProjectPurchasePrice { get; set; }
+        public double LossValue { get; set; }
         public double CostValue { get; set; }
         public double GainValue { get; set; }
-        public double LossValue { get; set; }
-        public double TotalBalance => (AssetValue + GainValue) - (CostValue + LossValue);
+        public double AssetValue { get; set; }
+        public double TotalBalance => (AssetValue + GainValue) - (ProjectPurchasePrice + LossValue + CostValue);
     }
 }
