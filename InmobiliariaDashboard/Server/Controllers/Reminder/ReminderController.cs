67dd6b78973e7b0e6ee8cfadcf1b9f2683ef4565
Marco Antonio <marco.3292@gmail.com>
2020-12-22 17:42:00 -0600

    Add reminder controllers

---
+++ InmobiliariaDashboard/Server/Controllers/Reminder/ReminderController.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using InmobiliariaDashboard.Server.Services;
+using InmobiliariaDashboard.Shared.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace InmobiliariaDashboard.Server.Controllers.Reminder
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class
+        ReminderController : BaseCatalogController<ReminderController, Models.Reminder, object, ReminderViewModel>
+    {
+        public ReminderController(ILogger<ReminderController> logger, IMapper mapper,
+            IBaseService<Models.Reminder, object> baseService)
+            : base(logger, mapper, baseService)
+        {
+        }
+    }
+}
---
+++ InmobiliariaDashboard/Server/Controllers/Reminder/ReminderDetailController.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using InmobiliariaDashboard.Server.Services;
+using InmobiliariaDashboard.Shared.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace InmobiliariaDashboard.Server.Controllers.Reminder
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class
+        ReminderDetailController : BaseDetailController<ReminderDetailController, Models.Reminder, object,
+            ReminderViewModel>
+    {
+        public ReminderDetailController(ILogger<ReminderDetailController> logger, IMapper mapper,
+            IBaseService<Models.Reminder, object> baseService) : base(logger, mapper, baseService)
+        {
+        }
+    }
+}
