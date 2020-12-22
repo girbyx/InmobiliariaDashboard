3c09cac76f7cd884301a300f0f5df1ab456e03ee
Marco Antonio <marco.3292@gmail.com>
2020-12-22 17:41:31 -0600

    Minor enum fixes

--- InmobiliariaDashboard/Shared/Enumerations/ProjectTypeEnum.cs
+++ InmobiliariaDashboard/Shared/Enumerations/ProjectTypeEnum.cs
@@ -5,7 +5,7 @@
         public static readonly ProjectTypeEnum FixedAsset = new ProjectTypeEnum(1, "I", "Inmueble");
         public static readonly ProjectTypeEnum MovableAsset = new ProjectTypeEnum(2, "M", "Mueble");
         public static readonly ProjectTypeEnum Office = new ProjectTypeEnum(3, "O", "Oficina");
-        public static readonly ProjectTypeEnum Other = new ProjectTypeEnum(3, "X", "Otro");
+        public static readonly ProjectTypeEnum Other = new ProjectTypeEnum(4, "X", "Otro");
 
         public ProjectTypeEnum() { }
         public ProjectTypeEnum(int id, string code, string name)
