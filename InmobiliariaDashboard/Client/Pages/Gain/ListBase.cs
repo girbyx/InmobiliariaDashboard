bc984e5286ce483e366b8d03c7fc5f5430423281
Marco Antonio <marco.3292@gmail.com>
2020-12-22 15:49:55 -0600

    Add cost gain loss searches

--- InmobiliariaDashboard/Client/Pages/Gain/ListBase.cs
+++ InmobiliariaDashboard/Client/Pages/Gain/ListBase.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using InmobiliariaDashboard.Shared.ViewModels;
 using Microsoft.AspNetCore.Components;
@@ -8,11 +9,21 @@ namespace InmobiliariaDashboard.Client.Pages.Gain
     public class ListBase : ComponentBase
     {
         [Inject] public IService Service { get; set; }
+        public IEnumerable<GainViewModel> OriginalRecords;
         public IEnumerable<GainViewModel> Records;
+
+        protected async Task FilterRecords(ChangeEventArgs e)
+        {
+            var searchValue = e.Value.ToString().ToLower();
+            Records = OriginalRecords.Where(x =>
+                x.ProjectName.ToLower().Contains(searchValue)
+                || x.GainTypeName.ToLower().Contains(searchValue)
+                || x.MonetaryAgentName.ToLower().Contains(searchValue));
+        }
 
         protected override async Task OnInitializedAsync()
         {
-            Records = await Service.GetList();
+            Records = OriginalRecords = await Service.GetList();
         }
     }
 }
--- InmobiliariaDashboard/Client/Pages/Loss/ListBase.cs
+++ InmobiliariaDashboard/Client/Pages/Loss/ListBase.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using InmobiliariaDashboard.Shared.ViewModels;
 using Microsoft.AspNetCore.Components;
@@ -8,11 +9,21 @@ namespace InmobiliariaDashboard.Client.Pages.Loss
     public class ListBase : ComponentBase
     {
         [Inject] public IService Service { get; set; }
+        public IEnumerable<LossViewModel> OriginalRecords;
         public IEnumerable<LossViewModel> Records;
+
+        protected async Task FilterRecords(ChangeEventArgs e)
+        {
+            var searchValue = e.Value.ToString().ToLower();
+            Records = OriginalRecords.Where(x =>
+                x.ProjectName.ToLower().Contains(searchValue)
+                || x.LossTypeName.ToLower().Contains(searchValue)
+                || x.MonetaryAgentName.ToLower().Contains(searchValue));
+        }
 
         protected override async Task OnInitializedAsync()
         {
-            Records = await Service.GetList();
+            Records = OriginalRecords = await Service.GetList();
         }
     }
 }
