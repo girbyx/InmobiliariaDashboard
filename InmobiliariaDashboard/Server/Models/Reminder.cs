381348542cabf071f6b2c1889560f02d800b3df6
Marco Antonio <marco.3292@gmail.com>
2020-12-22 17:41:50 -0600

    Add model view model and enum

---
+++ InmobiliariaDashboard/Server/Models/Reminder.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using AutoMapper;
+using InmobiliariaDashboard.Server.Models.Interfaces;
+using InmobiliariaDashboard.Shared.ViewModels;
+
+namespace InmobiliariaDashboard.Server.Models
+{
+    public class Reminder : IIdentityFields, IAuditFields
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime RecurrentOn { get; set; }
+        public string ReminderType { get; set; }
+        public string Description { get; set; }
+
+        // audit & relationships
+        public int EnterpriseId { get; set; }
+        public virtual Enterprise Enterprise { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime? UpdatedOn { get; set; }
+        public string UpdatedBy { get; set; }
+
+        // collections
+        public virtual ICollection<Gain> Gains { get; set; }
+    }
+
+    public class ReminderProfile : Profile
+    {
+        public ReminderProfile()
+        {
+            CreateMap<Reminder, ReminderViewModel>();
+            CreateMap<ReminderViewModel, Reminder>();
+        }
+    }
+}
---
+++ InmobiliariaDashboard/Shared/ViewModels/ReminderViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace InmobiliariaDashboard.Shared.ViewModels
+{
+    public class ReminderViewModel
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public DateTime RecurrentOn { get; set; }
+        [Required]
+        public string ReminderType { get; set; }
+        public string Description { get; set; }
+    }
+}
