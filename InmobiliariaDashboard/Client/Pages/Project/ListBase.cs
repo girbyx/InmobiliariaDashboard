9cf1e560af84b686c9d52b9a451eec1b11853e33
Marco Antonio <marco.3292@gmail.com>
2020-12-22 15:42:08 -0600

    Base search functionality done for projects

--- InmobiliariaDashboard/Client/Pages/Project/ListBase.cs
+++ InmobiliariaDashboard/Client/Pages/Project/ListBase.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using InmobiliariaDashboard.Shared.ViewModels;
 using Microsoft.AspNetCore.Components;
@@ -8,11 +9,21 @@ namespace InmobiliariaDashboard.Client.Pages.Project
     public class ListBase : ComponentBase
     {
         [Inject] public IService Service { get; set; }
+        public IEnumerable<ProjectViewModel> OriginalRecords;
         public IEnumerable<ProjectViewModel> Records;
+
+        protected async Task FilterRecords(ChangeEventArgs e)
+        {
+            var searchValue = e.Value.ToString().ToLower();
+            Records = OriginalRecords.Where(x =>
+                x.Name.ToLower().Contains(searchValue)
+                || x.Code.ToLower().Contains(searchValue)
+                || x.EnterpriseName.ToLower().Contains(searchValue));
+        }
 
         protected override async Task OnInitializedAsync()
         {
-            Records = await Service.GetList();
+            Records = OriginalRecords = await Service.GetList();
         }
     }
 }
