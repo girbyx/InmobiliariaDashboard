5026ba9f6f781a5f80277662710846ef7bab1966
Marco Antonio <marco.3292@gmail.com>
2020-12-22 18:22:07 -0600

    Add reminder views

---
+++ InmobiliariaDashboard/Client/Pages/Reminder/AddBase.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using InmobiliariaDashboard.Shared;
+using InmobiliariaDashboard.Shared.Enumerations;
+using InmobiliariaDashboard.Shared.ViewModels;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+namespace InmobiliariaDashboard.Client.Pages.Reminder
+{
+    public class AddBase : ComponentBase
+    {
+        [Inject] public IService Service { get; set; }
+        public ReminderViewModel Record { get; set; } = new ReminderViewModel();
+        public IEnumerable<ReminderFrequencyEnum> ReminderFrequencies => BaseEnumeration.GetAll<ReminderFrequencyEnum>();
+
+        protected async Task HandleValidSubmit()
+        {
+            await Service.Add(Record);
+            await Service.Return();
+        }
+
+        protected async Task OnCancelClick(MouseEventArgs e)
+        {
+            await Service.Return();
+        }
+    }
+}
---
+++ InmobiliariaDashboard/Client/Pages/Reminder/Service.cs
@@ -0,0 +1,21 @@
+﻿using System.Net.Http;
+using InmobiliariaDashboard.Shared.ViewModels;
+using Microsoft.AspNetCore.Components;
+
+namespace InmobiliariaDashboard.Client.Pages.Reminder
+{
+    public interface IService : IBaseCatalogService<ReminderViewModel>
+    {
+
+    }
+
+    public class Service : BaseCatalogService<ReminderViewModel>, IService
+    {
+        public Service(HttpClient httpClient, NavigationManager navigationManager)
+            : base(httpClient, navigationManager)
+        {
+            ControllerName = "Reminder";
+            DetailControllerName = "ReminderDetail";
+        }
+    }
+}
