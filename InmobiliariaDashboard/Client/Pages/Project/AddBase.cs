6dc715829a24858db0af0ae077e11b34469a8938
Marco Antonio <marco.3292@gmail.com>
2021-01-11 22:40:34 -0600

    Minor style fixes

--- InmobiliariaDashboard/Client/Pages/Project/AddBase.cs
+++ InmobiliariaDashboard/Client/Pages/Project/AddBase.cs
@@ -17,15 +17,19 @@ namespace InmobiliariaDashboard.Client.Pages.Project
         public bool Saving { get; set; }
         public ProjectViewModel Record { get; set; } = new ProjectViewModel();
         public IEnumerable<ProjectTypeEnum> ProjectTypes => BaseEnumeration.GetAll<ProjectTypeEnum>();
+        public IEnumerable<ProjectSubTypeViewModel> ProjectSubTypes { get; set; }
         public IBrowserFile[] Files { get; set; }
 
         protected override async Task OnInitializedAsync()
         {
             Record = await Service.GetEmpty();
+            ProjectSubTypes = Record.ProjectSubTypes;
 
             // defaults
             Record.StartDate = DateTime.Today;
             Record.ProjectType = ProjectTypeEnum.FixedAsset.Code;
+            ProjectSubTypes = Record.ProjectSubTypes.Where(x => x.ProjectType == Record.ProjectType);
+            Record.ProjectSubTypeId = Record.ProjectSubTypes.FirstOrDefault(x => x.ProjectType == Record.ProjectType)?.Id ?? 0;
         }
 
         protected async Task HandleFileSelection(InputFileChangeEventArgs e)
@@ -41,6 +45,12 @@ namespace InmobiliariaDashboard.Client.Pages.Project
                 await Service.AddFiles(id, Files);
             Saving = false;
             await Service.Return();
+        }
+
+        protected async Task OnProjectTypeChange(string code)
+        {
+            ProjectSubTypes = Record.ProjectSubTypes.Where(x => x.ProjectType == code);
+            Record.ProjectSubTypeId = Record.ProjectSubTypes.FirstOrDefault(x => x.ProjectType == code)?.Id ?? 0;
         }
 
         protected async Task OnCancelClick(MouseEventArgs e)
