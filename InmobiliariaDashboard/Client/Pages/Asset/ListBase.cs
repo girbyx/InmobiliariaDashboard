264c124429de5d0d6e7bd589f5b28b5415a83f4a
Marco Antonio <marco.3292@gmail.com>
2021-01-11 23:14:43 -0600

    Fix asset list style and filters

--- InmobiliariaDashboard/Client/Pages/Asset/ListBase.cs
+++ InmobiliariaDashboard/Client/Pages/Asset/ListBase.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using InmobiliariaDashboard.Shared.ViewModels;
 using Microsoft.AspNetCore.Components;
@@ -8,11 +9,31 @@ namespace InmobiliariaDashboard.Client.Pages.Asset
     public class ListBase : ComponentBase
     {
         [Inject] public IService Service { get; set; }
+        public IEnumerable<AssetViewModel> OriginalRecords;
         public IEnumerable<AssetViewModel> Records;
+
+        protected async Task FilterRecords(ChangeEventArgs e)
+        {
+            var searchValue = e.Value.ToString().ToLower();
+            Records = OriginalRecords.Where(x =>
+                x.EnterpriseName.ToLower().Contains(searchValue)
+                || x.Name.ToLower().Contains(searchValue)
+                || x.Value.ToString("C").ToLower().Contains(searchValue)
+                || x.Quantity.ToString("N").ToLower().Contains(searchValue)
+                || x.SubTotal.ToString("C").ToLower().Contains(searchValue)
+                || x.AssetTypeName.ToLower().Contains(searchValue));
+        }
 
         protected override async Task OnInitializedAsync()
         {
-            Records = await Service.GetList();
+            Records = OriginalRecords = await Service.GetList();
+        }
+
+        protected async Task OnDeleteClick(int id)
+        {
+            await Service.Delete(id);
+            OriginalRecords = OriginalRecords.Where(x => x.Id != id);
+            Records = Records.Where(x => x.Id != id);
         }
     }
 }
