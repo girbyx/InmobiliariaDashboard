a26487ec91fd0ab52950579e71fcd67a6928cb1f
Marco Antonio <marco.3292@gmail.com>
2021-01-20 01:24:11 -0600

    Add cost and gain save service

--- InmobiliariaDashboard/Server/Services/CostService.cs
+++ InmobiliariaDashboard/Server/Services/CostService.cs
@@ -14,12 +14,55 @@ namespace InmobiliariaDashboard.Server.Services
 
     public class CostService : BaseService<Cost, object>, ICostService
     {
-        private readonly IApplicationDbContext _dbContext;
+        private const string TransferenceGuessLower = "transferencia";
+        private const string TransferenceGuess = "Transferencia";
 
-        public CostService(IApplicationDbContext dbContext, IMapper mapper, IConfiguration configuration) : base(
-            dbContext, mapper, configuration)
+        private readonly IApplicationDbContext _dbContext;
+        private readonly IMapper _mapper;
+
+        public CostService(IApplicationDbContext dbContext, IMapper mapper, IConfiguration configuration)
+            : base(dbContext, mapper, configuration)
         {
             _dbContext = dbContext;
+            _mapper = mapper;
+        }
+
+        public override int Save(Cost entity, out int id)
+        {
+            if (entity.Id == 0)
+                _dbContext.Add(entity);
+            else
+                _dbContext.Update(entity);
+
+            _dbContext.SaveChanges();
+            id = entity.Id;
+
+            if (entity.Transfer)
+            {
+                var gain = _mapper.Map<Gain>(entity);
+
+                // check transfer type
+                var gainType = _dbContext.Set<GainType>()
+                    .FirstOrDefault(x => x.Name.ToLower() == TransferenceGuessLower ||
+                                         x.Description.ToLower().Contains(TransferenceGuessLower));
+
+                if (gainType == null)
+                {
+                    var newGainType = new GainType { Name = TransferenceGuess, Description = TransferenceGuess };   
+                    _dbContext.Add(newGainType);
+                    _dbContext.SaveChanges();
+                    gain.GainTypeId = newGainType.Id;
+                }
+                else
+                    gain.GainTypeId = gainType.Id;
+
+                gain.BankAccountId = entity.GainBankAccountId ?? 0;
+                gain.CostBankAccountId = entity.Id;
+
+                _dbContext.Add(gain);
+            }
+
+            return _dbContext.SaveChanges();
         }
 
         public override Cost Get(int id)
--- InmobiliariaDashboard/Server/Services/GainService.cs
+++ InmobiliariaDashboard/Server/Services/GainService.cs
@@ -3,6 +3,7 @@ using AutoMapper;
 using InmobiliariaDashboard.Server.Data;
 using InmobiliariaDashboard.Server.Entities;
 using InmobiliariaDashboard.Shared;
+using InmobiliariaDashboard.Shared.Enumerations;
 using Microsoft.Extensions.Configuration;
 
 namespace InmobiliariaDashboard.Server.Services
@@ -13,10 +14,56 @@ namespace InmobiliariaDashboard.Server.Services
 
     public class GainService : BaseService<Gain, object>, IGainService
     {
+        private const string TransferenceGuessLower = "transferencia";
+        private const string TransferenceGuess = "Transferencia";
 
-        public GainService(IApplicationDbContext dbContext, IMapper mapper, IConfiguration configuration) : base(
-            dbContext, mapper, configuration)
+        private readonly IApplicationDbContext _dbContext;
+        private readonly IMapper _mapper;
+
+        public GainService(IApplicationDbContext dbContext, IMapper mapper, IConfiguration configuration)
+            : base(dbContext, mapper, configuration)
         {
+            _dbContext = dbContext;
+            _mapper = mapper;
+        }
+
+        public override int Save(Gain entity, out int id)
+        {
+            if (entity.Id == 0)
+                _dbContext.Add(entity);
+            else
+                _dbContext.Update(entity);
+
+            _dbContext.SaveChanges();
+            id = entity.Id;
+
+            if (entity.Transfer)
+            {
+                var cost = _mapper.Map<Cost>(entity);
+
+                // check transfer type
+                var costType = _dbContext.Set<GainType>()
+                    .FirstOrDefault(x => x.Name.ToLower() == TransferenceGuessLower ||
+                                         x.Description.ToLower().Contains(TransferenceGuessLower));
+
+                if (costType == null)
+                {
+                    var newCostType = new CostType { Name = TransferenceGuess, Description = TransferenceGuess };
+                    _dbContext.Add(newCostType);
+                    _dbContext.SaveChanges();
+                    cost.CostTypeId = newCostType.Id;
+                }
+                else
+                    cost.CostTypeId = costType.Id;
+
+                cost.CommissionType = CommissionTypeEnum.Money.Code;
+                cost.BankAccountId = entity.CostBankAccountId ?? 0;
+                cost.GainBankAccountId = entity.Id;
+
+                _dbContext.Add(cost);
+            }
+
+            return _dbContext.SaveChanges();
         }
 
         public override int SaveAttachments(string[] files, int costId)
